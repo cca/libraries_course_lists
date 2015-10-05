d6057f0f8cd7824d1ee6a8e306a52bc1342037aa
phette23 <phette23@gmail.com>
2015-10-05 11:01:35 -0700

    delete semester - one more if condition, silences some errors

--- delete-all-of-a-semester.fish
+++ delete-all-of-a-semester.fish
@@ -16,16 +16,21 @@ set depts 'ARCH DIV' $depts
 
 for dept in $depts
     set taxoID (eq tax --name "$dept - COURSE LIST" | jq -r '.uuid')
-    # couldn't find an easy way to filter to object where
-    # .term == $semester with jq, so we store all IDs & iterate over them
-    set termIDs (eq tax "$taxoID/term" | jq -r '.[].uuid')
-    if [ (count $termIDs) -gt 0 ]
-        for id in $termIDs
-            set term "$taxoID/term/$id"
-            if [ (eq tax $term | jq -r '.term') = $semester ]
-                # DELETE returns "undefined" on success so we silence that
-                eq --method del tax $term > /dev/null
-                and echo "deleted $semester from $dept - COURSE LIST"
+
+    if [ $taxoID ]
+        # couldn't find an easy way to filter to object where
+        # .term == $semester with jq, so we store all IDs & iterate over them
+        set termIDs (eq tax "$taxoID/term" | jq -r '.[].uuid')
+
+        if [ (count $termIDs) -gt 0 ]
+            for id in $termIDs
+                set term "$taxoID/term/$id"
+
+                if [ (eq tax $term | jq -r '.term') = $semester ]
+                    # DELETE returns "undefined" on success so we silence that
+                    eq --method del tax $term > /dev/null
+                    and echo "deleted $semester from $dept - COURSE LIST"
+                end
             end
         end
     end
