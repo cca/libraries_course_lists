e202868eaac12cbc5be95f59249f655225ca0bc2
phette23 <phette23@gmail.com>
2016-01-13 15:44:47 -0800

    clean up tax/uuid code, logging

--- syllabus-collection.fish
+++ syllabus-collection.fish
@@ -21,62 +21,69 @@ csvcut -c 7 $filename | tail -n +2 | sort | uniq  | sed -e '/""/d' > $dir/$dept-
 and echo "Wrote $dept XList IDs CSV…"
 csvcut -c 2 $filename | tail -n +2 | sort | uniq  > $dir/$dept-dept-codes.csv
 and echo "Wrote $dept department codes CSV…"
-exit
+
 # update all taxonomies
 # main course list
-set uuid (eq tax --name "$dept - COURSE LIST" | jq -r '.uuid')
+set tax "$dept - COURSE LIST"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 # @TODO should a script delete current semester prior?
 # place to do it would be in "delete-all-of-a-semester.fish" script
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-course-list-taxo.csv >> $logfile
 end
 
-# course titles
-set uuid (eq tax --name "$dept - course titles" | jq -r '.uuid')
+# course titles e.g. "Introduction to Painting"
+set tax "$dept - course titles"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-course-titles.csv >> $logfile
 end
 
-# faculty names
-set uuid (eq tax --name "$dept - faculty" | jq -r '.uuid')
+# faculty names e.g. "Annemarie Haar, Eric Phetteplace"
+set tax "$dept - faculty"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-faculty-names.csv >> $logfile
 end
 
 # course names e.g. INDIV-101
-set uuid (eq tax --name "$dept - course names" | jq -r '.uuid')
+set tax "$dept - course names"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-courses.csv >> $logfile
 end
 
-# course sections
-set uuid (eq tax --name "$dept - course sections" | jq -r '.uuid')
+# course sections e.g. ANIMA-101-01
+set tax "$dept - course sections"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-section-names.csv >> $logfile
 end
 
 # Xlist IDs
-set uuid (eq tax --name "$dept - cross-list keys (XList)" | jq -r '.uuid')
+set tax "$dept - cross-list keys (XList)"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-xlist.csv >> $logfile
 end
 
-# deptartment codes
-set uuid (eq tax --name "$dept - dept codes" | jq -r '.uuid')
+# deptartment codes e.g. ANIMA
+set tax "$dept - dept codes"
+set uuid (eq tax --name $tax | jq -r '.uuid')
 if [ $uuid ]
-    log "UPDATING TAXONOMY ID $uuid" $logfile
+    log "Updating $tax taxonomy" $logfile
     uptaxo --tid $uuid --pw $pw --un $un \
         --csv $dir/$dept-dept-codes.csv >> $logfile
 end
--- upload-taxos-to-vault.fish
+++ upload-taxos-to-vault.fish
@@ -15,46 +15,52 @@ set un (jq -r '.username' ~/.equellarc)
 # parse all department codes listed in the report
 # trim header row with tail -n +2 (might need gnu tail not OS X?)
 set depts (csvcut -c 2 $filename | tail -n +2 | sort | uniq | \
-    # remove Architecture Division (handled separately) & Fine Arts critiques (no taxos)
-    sed -e '/ARCHT/d' -e '/INTER/d' -e '/MARCH/d' -e '/CRITI/d')
+    # remove Architecture Division (handled separately)
+    # plus Craft & Fine Arts critiques (no taxonomies for these)
+    sed -e '/ARCHT/d' -e '/INTER/d' -e '/MARCH/d' -e '/CRAFT/d' -e '/CRITI/d')
 
 for dept in $depts
-    # full course list
-    set uuid (eq tax --name "$dept - COURSE LIST" | jq -r '.uuid')
+    # full course list in EQUELLA taxonomy format
+    set tax "$dept - COURSE LIST"
+    set uuid (eq tax --name $tax | jq -r '.uuid')
     if [ $uuid ]
-        log "UPDATING TAXONOMY ID $uuid" $logfile
+        log "Updating $tax taxonomy" $logfile
         uptaxo --tid $uuid --pw $pw --un $un \
             --csv $dir/$dept-course-list-taxo.csv >> $logfile
     end
 
-    # course titles
-    set uuid (eq tax --name "$dept - course titles" | jq -r '.uuid')
+    # course titles e.g. "Introduction to Printmaking"
+    set tax "$dept - course titles"
+    set uuid (eq tax --name $tax | jq -r '.uuid')
     if [ $uuid ]
-        log "UPDATING TAXONOMY ID $uuid" $logfile
+        log "Updating $tax taxonomy" $logfile
         uptaxo --tid $uuid --pw $pw --un $un \
             --csv $dir/$dept-course-titles.csv >> $logfile
     end
 
-    # faculty names
-    set uuid (eq tax --name "$dept - faculty" | jq -r '.uuid')
+    # faculty names e.g. "Annemarie Haar, Eric Phetteplace"
+    set tax "$dept - faculty"
+    set uuid (eq tax --name $tax | jq -r '.uuid')
     if [ $uuid ]
-        log "UPDATING TAXONOMY ID $uuid" $logfile
+        log "Updating $tax taxonomy" $logfile
         uptaxo --tid $uuid --pw $pw --un $un \
             --csv $dir/$dept-faculty-names.csv >> $logfile
     end
 
     # course names e.g. INDIV-101
-    set uuid (eq tax --name "$dept - course names" | jq -r '.uuid')
+    set tax "$dept - course names"
+    set uuid (eq tax --name $tax | jq -r '.uuid')
     if [ $uuid ]
-        log "UPDATING TAXONOMY ID $uuid" $logfile
+        log "Updating $tax taxonomy" $logfile
         uptaxo --tid $uuid --pw $pw --un $un \
             --csv $dir/$dept-courses.csv >> $logfile
     end
 
-    # course sections
-    set uuid (eq tax --name "$dept - course sections" | jq -r '.uuid')
+    # course sections e.g. INDIV-101-01
+    set tax "$dept - course sections"
+    set uuid (eq tax --name $tax | jq -r '.uuid')
     if [ $uuid ]
-        log "UPDATING TAXONOMY ID $uuid" $logfile
+        log "Updating $tax taxonomy" $logfile
         uptaxo --tid $uuid --pw $pw --un $un \
             --csv $dir/$dept-section-names.csv >> $logfile
     end
