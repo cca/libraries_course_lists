e0d8f00765f74ce7ad18ef900c67b1b66343f627
Eric Phetteplace <phette23@gmail.com>
2021-08-12 10:27:18 -0700

    update log function to log all messages
    was just writing the first sentence of each message

--- log.fish
+++ log.fish
@@ -1,8 +1,6 @@
 # abstraction for writing messages to a provided log file
 function log -d 'write message to given log file'
-    set msg $argv[1]
     # default log file is just named with the date
     set -q LOGFILE || set LOGFILE logs/(date "+%Y-%m-%d").txt
-    set timestamp (date "+%Y-%m-%d %H:%M")
-    echo -e $timestamp '\t' $msg >> $LOGFILE
+    echo -e (date "+%Y-%m-%d %H:%M") '\t' $argv >> $LOGFILE
 end
