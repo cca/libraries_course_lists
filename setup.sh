0517db15f15cd63bc86c8ebd8700af67743654b2
phette23 <phette23@gmail.com>
2022-08-25 12:24:33 -0700

    initial version of setup script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+exists () {
+    command -v "$1" &>/dev/null
+}
+
+# https://brew.sh/
+exists brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+exists node || brew install node
+exists fish || brew install fish
+exists jq || brew install jq
+exists python3 || brew install python3
+# csvkit doesn't install a utility named csvkit, pick a random one to test
+exists csvjoin || brew install csvkit
+
+exists eq || npm i -g equella-cli
+
+# python scripts
+echo "Installing Python scripts to ~/bin — ensure that directory is on your PATH"
+mkdir -p ~/bin
+
+if ! test -e ~/bin/uptaxo; then
+    wget -O ~/bin/uptaxo https://gist.githubusercontent.com/phette23/9bec679b7b677af7e396e8a40e7a7047/raw/171af0a5dcaefff5bc3d0281faf3ed4183674495/uptaxo.py
+    chmod +x ~/bin/uptaxo
+fi
+if ! test -e ~/bin/equellasoap.py; then
+    wget -O ~/bin/equellasoap.py https://raw.githubusercontent.com/openequella/openequella.github.io/master/example-scripts/SOAP/python/equellasoap.py
+fi
+if ! test -e ~/bin/util.py; then
+    wget -O ~/bin/util.py https://raw.githubusercontent.com/openequella/openequella.github.io/master/example-scripts/SOAP/python/util.py
+fi
+
+exists python2 || echo "You must install Python 2.7 yourself: https://www.python.org/downloads/"
