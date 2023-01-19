8a4da7283c6509f53f2ab1ec4495553c2c324af8
phette23 <phette23@gmail.com>
2023-01-19 14:35:44 -0800

    improve delete...semester script
    figured out how to filter with jq to the term we want to delete
    rather than brute force iterating over all terms with API calls

--- delete-all-of-a-semester.fish
+++ delete-all-of-a-semester.fish
@@ -22,20 +22,13 @@ for dept in $depts
     set taxoID (eq tax --name "$dept - COURSE LIST" | jq -r '.uuid')
 
     if [ $taxoID ]
-        # couldn't find an easy way to filter to object where
-        # .term == $semester with jq, so we store all IDs & iterate over them
-        set termIDs (eq tax "$taxoID/term" | jq -r '.[].uuid')
+        set termID (eq tax "$taxoID/term" | jq -r ".[] | select(.term | contains("$semester")) | .uuid")
 
-        if [ (count $termIDs) -gt 0 ]
-            for id in $termIDs
-                set term "$taxoID/term/$id"
-
-                if [ (eq tax $term | jq -r '.term') = $semester ]
-                    # DELETE returns "undefined" on success so we silence that
-                    eq --method del tax $term > /dev/null
-                    and log "deleted $semester from $dept - COURSE LIST"
-                end
-            end
+        if [ -n $termID ]
+            eq --method del tax $term >/dev/null
+            and log "deleted $semester from $dept - COURSE LIST"
+        else
+            log "couldn't find \"$semester\" term in $dept - COURSE LIST taxo"
         end
     end
 end
