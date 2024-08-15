5582abbbaab081c4363428f9ba6a6d8ce6d9d139
phette23 <phette23@gmail.com>
2024-08-15 11:24:40 -0700

    fix: silence warning about dialect sniffing
    for some reason csvsort, and only csvsort on this particular line, has trouble detecting the CSV delimiter here so we make it explicit

--- get-columns.fish
+++ get-columns.fish
@@ -20,7 +20,7 @@ log "Wrote $dept course titles, faculty, sections, and courses CSVs…"
 
 # first sort file (first column is semester), then process
 # --no-inference REQUIRED b/c otherwise it translates "MARCH" to "9999-03-31"
-csvsort --no-inference $filename | tail -n +2 >tmp
+csvsort --delimiter "," --no-inference $filename | tail -n +2 >tmp
 mv tmp $filename
 if [ $dept = ARCHT -o $dept = BARCH -o $dept = INTER -o $dept = MARCH -o $dept = SYLLABUS ]
     ./course-csv-to-taxo.py --program $filename >data/$dept-course-list-taxo.csv
